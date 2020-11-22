ebbd42bae6402a54817c6d3e92cf52fe4b2baa55
Pete Lawrence <petejlawrence@icloud.com>
2020-11-22 11:41:50 +0000

    Tidying up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 
 import SessionsPage from './pages/SessionsPage';
 
