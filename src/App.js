c8703f24ada96fceac2f26b06a571835b234cb85
Pete Lawrence <petejlawrence@icloud.com>
2020-11-22 10:31:02 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+import SessionsPage from './pages/SessionsPage';
+
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Router>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand href="#home">AWS re:invent 2020</Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        </Navbar>
+
+        <Switch>
+
+          <Route exact path="/">
+            <SessionsPage />
+          </Route>
+
+        </Switch>
+
+      </Router>
+
+    </Container>
   );
 }
 
---
+++ src/pages/SessionsPage.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import dayjs from 'dayjs';
+import Card from 'react-bootstrap/Card';
+import { Form } from 'react-bootstrap';
+
+const data = require('../data/schedule.json');
+
+var advancedFormat = require('dayjs/plugin/advancedFormat');
+dayjs.extend(advancedFormat)
+
+class SessionsPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      language: 'english',
+      startTime: 9,
+      endTime: 17,
+      date: '2020-12-02'
+    };
+  }
+
+  getLanguages() {
+    return [ 'english', 'Spanish' ];
+  }
+
+  filterSessions() {
+    let sessions = [];
+
+    for (let session of data.sessions) {
+      let tags = session.tags.split(',');
+      let hiddenTags = session.hiddenTags.split(',');
+
+      // Langauge Filter
+      if (this.state.language) {
+        if (!tags.includes(this.state.language) && !hiddenTags.includes(this.state.language)) {
+          continue;
+        }
+      }
+
+      // Date filter
+      if (this.state.date) {
+        let sessionStart = dayjs.unix(session.schedulingData.start.timestamp);
+        if (!sessionStart.isSame(this.state.date, 'day')) {
+          continue;
+        }
+      }
+
+      // Time filter
+      if (this.state.startTime && this.state.endTime) {
+        let sessionStart = dayjs.unix(session.schedulingData.start.timestamp);
+        console.log(sessionStart.format('H'), this.state.startTime, this.state.endTime);
+        if (sessionStart.format('H') < parseInt(this.state.startTime) || sessionStart.format('H') > parseInt(this.state.endTime)) {
+          continue;
+        }
+      }
+
+      sessions.push(session);
+    }
+
+    return sessions;
+  }
+
+  listSessions() {
+    let sessions = this.filterSessions();
+
+    return sessions.map(session => {
+      let sessionStart = dayjs.unix(session.schedulingData.start.timestamp);
+      let sessionEnd = dayjs.unix(session.schedulingData.end.timestamp);
+
+      return(
+        <Card className="mb-2">
+          <Card.Body>
+            <Card.Title>{ session.name }</Card.Title>
+            <Card.Subtitle>{ sessionStart.format('Do MMM HH:mm') } - { sessionEnd.format('HH:mm') }</Card.Subtitle>
+            <div dangerouslySetInnerHTML={{ __html: session.description }} />
+          </Card.Body>
+        </Card>
+      )
+    });
+  }
+
+  handleLanguageChange(event) {
+    this.setState({
+      language: event.target.value
+    });
+  }
+
+  handleStartTimeChange(event) {
+    this.setState({
+      startTime: event.target.value
+    });
+  }
+
+  handleEndTimeChange(event) {
+    this.setState({
+      endTime: event.target.value
+    });
+  }
+
+  handleDateChange(event) {
+    this.setState({
+      date: event.target.value
+    });
+  }
+
+
+  /**
+   * Renders the Marker component
+   * @return {[type]} [description]
+   */
+  render() {
+    return (
+      <>
+        <h1>Sessions</h1>
+
+        <Form>
+          <Form.Group>
+            <Form.Label>Language</Form.Label>
+            <Form.Control as="select" ref="language" onChange={this.handleLanguageChange.bind(this)} >
+              { this.getLanguages().map(lang => ( <option>{ lang }</option> ) ) }
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Date</Form.Label>
+            <Form.Control type="date" defaultValue="2020-12-02" onChange={ this.handleDateChange.bind(this)} />
+          </Form.Group>
+          
+          <Form.Group>
+            <Form.Label>Time</Form.Label>
+            <Form.Control type="time" defaultValue="9" onChange={ this.handleStartTimeChange.bind(this)} />
+            <Form.Control type="time" defaultValue="17" onChange={ this.handleEndTimeChange.bind(this)}/>
+          </Form.Group>
+        </Form>
+
+          { this.listSessions(this.state.language) }
+      </>
+    );
+  }
+}
+export default SessionsPage;
